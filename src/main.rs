a7f22cdbf3bae1257098bb96dc3cd27c65277a19
Vinícius Andrade <vinivice@gmail.com>
2023-10-22 19:09:49 -0300

    Working v1.0.0

--- src/main.rs
+++ src/main.rs
@@ -4,61 +4,64 @@ use enigo::*;
 static mut SPEED: i32 = 20;
 static mut ACTIVE: bool = false;
 fn main() {
-    if let Err(error) = grab(callback) {
-        println!("Error: {:?}", error)
+    if let Err(_) = grab(callback) {
+      //  println!("Error: {:?}", error)
+        std::process::exit(exitcode::SOFTWARE);
     }
 }
 
 fn callback(event: Event) -> Option<Event> {
-    /*println!("My callback {:?}", event);
-    match event.name {
-        Some(string) => println!("User wrote {:?}", string),
-        None => (),
-    }*/
     let mut enigo = Enigo::new();
-    //println!("My callback {:?}", event);
     match event.event_type {
         EventType::KeyRelease(rdev::Key::ScrollLock) => { 
             unsafe {
                 ACTIVE = !ACTIVE;
-      //          println!("{}", ACTIVE);
             }
         }
         EventType::KeyPress(k) => unsafe {
-            //println!("Event {:?}", k);
             if ACTIVE {
                 match k {
                     rdev::Key::Kp1 => { 
-                        println!("k1");
                         enigo.mouse_move_relative(-SPEED, 0);
+                        return None;
                     }
                     rdev::Key::Kp2 => { 
-                        println!("k2");
                         enigo.mouse_move_relative(0, SPEED);
+                        return None;
                     }
 
                     rdev::Key::Kp3 => { 
-                        println!("k3");
                         enigo.mouse_move_relative(SPEED, 0);
+                        return None;
+                    }
+
+                    rdev::Key::Kp4 => { 
+                        enigo.mouse_click(MouseButton::Left);
+                        return None;
                     }
 
                     rdev::Key::Kp5 => { 
-                        println!("k5");
                         enigo.mouse_move_relative(0, -SPEED);
+                        return None;
+                    }
+                    rdev::Key::Kp6 => { 
+                        enigo.mouse_click(MouseButton::Right);
+                        return None;
                     }
 
-                    rdev::Key::KpPlus => SPEED += 5,
+                    rdev::Key::KpPlus => {
+                        SPEED += 5;
+                        return None;
+                    }
                     rdev::Key::KpMinus => {
                         SPEED -= 5;
                         if SPEED < 0 { SPEED = 0; }
+                        return None;
                     }
                     _ => (),
                 }
             }
         }
-        //EventType::MouseMove => { 
-         //   println!(event.x, event.y);
-        //}
         _ => (),
     }
     return Some(event)
